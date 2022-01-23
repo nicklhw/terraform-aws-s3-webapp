98e71af610093f5789ee4ce6c893f83265d6a5e9
nicklhw <nicklhw@gmail.com>
2022-01-23 16:58:22 -0500

    wip

--- variables.tf
+++ variables.tf
@@ -1,3 +1,12 @@
-variable "region" {}
-variable "prefix" {}
-variable "name" {}
+variable "region" {
+  type    = string
+  default = "us-east-1"
+}
+variable "prefix" {
+  type    = string
+  default = "dev"
+}
+variable "name" {
+  type    = string
+  default = "nwong"
+}
