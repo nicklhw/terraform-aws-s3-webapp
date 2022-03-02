9d68f662af62da1eecd3b4f71d582cab60a0f92a
nicklhw <nicklhw@gmail.com>
2022-03-02 15:12:17 -0500

    format

--- variables.tf
+++ variables.tf
@@ -2,10 +2,12 @@ variable "region" {
   type    = string
   default = "us-east-1"
 }
+
 variable "prefix" {
   type    = string
   default = "dev"
 }
+
 variable "name" {
   type    = string
   default = "nwong"
